25fa1c2ab004e364fcc85cd5996b4f1018d9761a
yuricooke <yuri.cooke@ensineme.com.br>
2023-05-23 17:52:14 -0300

    new_files

---
+++ this/app.js
@@ -0,0 +1,29 @@
+class NameField {
+    constructor(name) {
+        const field = document.createElement('li');
+        field.textContent = name;
+        const nameListHook = document.querySelector('#names');
+        nameListHook.appendChild(field);
+    }
+}
+
+class NameGenerator {
+    constructor() {
+        const btn = document.querySelector('button');
+        this.names = ['Max','Manu','Anna'];
+        this.currentName = 0;
+        btn.addEventListener('click', () => {
+            this.addName();
+        });
+    }
+
+    addName() {
+        const name = new NameField(this.names[this.currentName]);
+        this.currentName++
+        if (this.currentName >= this.names.length) {
+            this.currentName = 0;
+        }
+    }
+}
+
+const gen = new NameGenerator();
